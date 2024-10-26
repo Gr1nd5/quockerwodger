2646e2b63a26759c7df562c8fc47b4b18b28d6cb
Gr1nd5 <56499823+Gr1nd5@users.noreply.github.com>
2024-10-26 15:05:23 -0700

    Vercel adding

--- server.js
+++ server.js
@@ -1,27 +1,21 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 5000; // Use the PORT provided by Render
+const PORT = process.env.PORT || 5000;
 
-// Middleware to serve static files
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Middleware to parse JSON
 app.use(express.json());
 
-// Route to serve the HTML file
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-// Endpoint to handle form submissions
 app.post('/send', (req, res) => {
     const { name, email, message } = req.body;
     console.log(`Name: ${name}, Email: ${email}, Message: ${message}`);
     res.status(200).send('Message received');
 });
 
-// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
