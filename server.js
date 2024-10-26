325239d5c88908a3d5d88a38589a2144ee09f2f4
Gr1nd5 <56499823+Gr1nd5@users.noreply.github.com>
2024-10-26 16:08:53 -0700

    updated

--- server.js
+++ server.js
@@ -3,23 +3,19 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
-// Serve the HTML file
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-// Endpoint for form submissions
 app.post('/send', (req, res) => {
     const { name, email, message } = req.body;
     console.log(`Name: ${name}, Email: ${email}, Message: ${message}`);
     res.status(200).send('Message received');
 });
 
-// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
